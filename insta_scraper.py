903928313f9a1e06f3858d3be8e929e95539aae2
Paulo M C Lopes <42880691+reset-req@users.noreply.github.com>
2023-03-16 02:15:52 -0300

    Add targets and separate list of followers/ing
    You may now target other profiles to scrap, only work if those profiles are publics or connected with yours.

--- insta_scraper.py
+++ insta_scraper.py
@@ -3,38 +3,77 @@ import numpy as np
 
 class InstagramScraper:
 
-    def __init__(self, username, password):
+    def __init__(self, username, password, target=None, list=None):
         
         self.username = username
         self.password = password
-        self.profile = None
+        
+        if target is None:
+            self.target = username
+        else:
+            self.target = target
+
+        if list is None:
+            self.list = 'u'
+        else:
+            self.list = list
+            self.list.append('u')
+
         self.followers_list = []
         self.following_list = []
 
     def create_session(self):
 
-        L = instaloader.Instaloader()
+        L = instaloader.Instaloader(max_connection_attempts=1) # login errors may occur without it
         L.login(self.username, self.password) # Login or load session
-        self.profile = instaloader.Profile.from_username(L.context, self.username) # Obtain profile metadata
+        self.target = instaloader.Profile.from_username(L.context, self.target) 
+        
 
     def scrape_followers(self):
        
-        for follower in self.profile.get_followers():
+        for follower in self.target.get_followers():
             self.followers_list.append(follower.username)
 
     def scrape_following(self):
 
-        for followee in self.profile.get_followees():
+        for followee in self.target.get_followees():
             self.following_list.append(followee.username)
 
-    def generate_unfollowers_list(self):
+    def generate_list(self):
 
-        unfollow_list = np.setdiff1d(self.following_list, self.followers_list) # unfollow people who are only in following list and not in followers list
-        print("People to unfollow: ", unfollow_list)
-        filename = "unfollowers_" + self.username + ".txt"
-        file = open(filename, "w")
-        for person in unfollow_list:
-            file.write(person + "\n")
-        file.close()
+        for opts in self.list:    
+            
+            
+            match opts:
+                case 'fi':
+                    list = self.following_list
+                    name = "following_"
+                case 'fe':
+                    list = self.followers_list
+                    name = "followers_"
+                case 'u':
+                    list = np.setdiff1d(self.following_list, self.followers_list) 
+                    name = "unfollowing_"
+
+            filename = name + ".txt" 
+            file = open(filename, "w")
+            print("People in list: ", list)
+            for person in list:
+                file.write(person + "\n")
+            file.close()
 
 
+
+
+        # Extra description:
+        
+        # unfollow people who are only in following list and not in followers list
+        # now obtain profile metadata from target (before was username)
+
+        ##########################################################################
+        # Backlog:
+
+        # there is probably a better way to do target is none...
+        # choose better names for flas
+        # improve file names
+        # save session
